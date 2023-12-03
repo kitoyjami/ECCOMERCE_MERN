7769c70d9e40c956f1aa13323c58fe0c86863f06
kitoyjami <weurbanor@unitru.edu.pe>
2023-12-03 15:32:15 -0500

    se agregaron controles y crud

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,3 +1,4 @@
+const { generateToken } = require("../config/jwtToken")
 const User=require("../models/userModel")
 const asyncHandler=require('express-async-handler')
 
@@ -19,5 +20,85 @@ const createUser =asyncHandler(async (req,res) => {
 
 })
 
+const loginUserCtlr=asyncHandler(async (req,res)=> {
+    const {email,password} =req.body
+    // check if user exist or not 
+    const findUser=await User.findOne({email})
+    if(findUser && await findUser.isPasswordMatched(password))
+    {
+        res.json({
+            _id : findUser?.id,
+            firstname: findUser?.firstname,
+            lasttname: findUser?.lasttname,
+            email : findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?._id)
 
-module.exports = {createUser}
+        })
+    }else {
+        throw new Error("Invalid Crendential")
+    }
+})
+
+// GET ALL USERS
+
+const getAllUser= asyncHandler(async (req,res)=>{
+    console.log(req.params)
+    try {
+        const getUsers=await User.find()
+        res.json(getUsers)
+    }catch(error){
+        throw new Error(error)
+    }
+})
+
+const getaUser= asyncHandler(async (req,res)=>{
+    const {id} =req.params
+    try {
+        
+        const getaUser=await User.findById(id)
+        res.json({
+            getaUser
+        })
+    }catch(error){
+        throw new Error(error)
+    }
+})
+
+const deleteaUser= asyncHandler(async (req,res)=>{
+    const {id} =req.params
+    try {
+        
+        const deleteaUser=await User.findByIdAndDelete(id)
+        res.json({
+            deleteaUser
+        })
+    }catch(error){
+        throw new Error(error)
+    }
+})
+
+const updateaUser= asyncHandler(async (req,res)=>{
+    const {id} =req.params
+    try {
+        
+        const updateaUser=await User.findByIdAndUpdate(id,{
+            firstname: req?.body?.firstname,
+            lasttname: req?.body?.lasttname,
+            email:  req?.body?.email,
+            mobile: req?.body?.mobile
+        },
+        {
+            new:true,
+
+        })
+        res.json({
+            updateaUser
+        })
+    }catch(error){
+        throw new Error(error)
+    }
+})
+
+
+module.exports = {createUser,loginUserCtlr,getAllUser,getaUser,deleteaUser,updateaUser}
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const { createUser } = require('../controller/userCtrl')
+const { createUser, loginUserCtlr, getAllUser, getaUser, deleteaUser, updateaUser } = require('../controller/userCtrl')
 const router = express.Router()
 
 createUser
 
 
 router.post("/register",createUser)
-
+router.post("/login",loginUserCtlr)
+router.get("/users",getAllUser)
+router.get("/:id",getaUser)
+router.delete("/:id",deleteaUser)
+router.put("/:id",updateaUser)
 module.exports=router
