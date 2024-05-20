11c54449d4eb5f19d94405a37749da336499e430
kitoyjami <weurbanor@unitru.edu.pe>
2024-05-20 17:17:36 -0500

    dsadsa

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -58,7 +58,7 @@ router.put("/password",authMiddleware,updatePassword)
 
 router.put("/block-user/:id",authMiddleware,isAdmin,blockUser)
 router.put("/unblock-user/:id",authMiddleware,isAdmin,unblockUser)
-router.post("/logout",logout)    
+router.post("/logout",authMiddleware,logout)    
 
 
 module.exports=router
