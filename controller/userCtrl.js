c0eccf27542259c6936753d94dea9a963fc88428
kitoyjami <weurbanor@unitru.edu.pe>
2024-05-21 01:33:21 -0500

    lastname

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -48,7 +48,7 @@ const loginUserCtlr = asyncHandler(async (req, res) => {
         res.json({
             _id: findUser.id,
             firstname: findUser.firstname,
-            lastname: findUser.lastname,
+            lasttname: findUser.lastname,
             email: findUser.email,
             mobile: findUser.mobile,
             photo: findUser.photo,
