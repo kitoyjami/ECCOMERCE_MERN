0e2c63115fe8ce0faa804fd210eb386df29bd0fc
kitoyjami <weurbanor@unitru.edu.pe>
2024-06-12 15:37:34 -0500

    add

--- controller/rendicionCuentaCtrl.js
+++ controller/rendicionCuentaCtrl.js
@@ -3,8 +3,33 @@ const RendicionCuenta = require('../models/rendicionCuentaModel');
 const Servicio = require('../models/servicioModel');
 
 // Crear una nueva rendición de cuenta
-const createRendicionCuenta = async (req, res) => {
-  try {
+const createRendicionCuenta = asyncHandler(async (req, res) => {
+  const {
+    fecha,
+    tipoComprobante,
+    foto,
+    estado,
+    proveedor,
+    descripcionComprobante,
+    agregarIGV,
+    nroComprobante,
+    moneda,
+    tipoCambio  
+  } = req.body;
+
+  const nuevaRendicionCuenta = new RendicionCuenta({
+    fecha,
+    tipoComprobante,
+    foto,
+    estado,
+    proveedor,
+    descripcionComprobante,
+    agregarIGV,
+    registradoPor: { user: req.user._id }
+  });
+
+  await nuevaRendicionCuenta.save();
+  try{
     const {
       fecha,
       tipoComprobante,
@@ -33,11 +58,22 @@ const createRendicionCuenta = async (req, res) => {
     });
 
     await nuevaRendicionCuenta.save();
-    res.status(201).json(nuevaRendicionCuenta);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
+     // Actualizar el registro de gastos en el servicio correspondiente
+  const serviciosIds = descripcionComprobante.map(desc => desc.servicio);
+  await Servicio.updateMany(
+    { _id: { $in: serviciosIds } },
+    { $push: { registroGastos: nuevaRendicionCuenta._id } }
+  );
+
+  res.status(201).json(nuevaRendicionCuenta);
+
+  }catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+
+});
+
+
 
 // Obtener todas las rendiciones de cuenta
 const getAllRendicionesCuenta = asyncHandler(async (req, res) => {
