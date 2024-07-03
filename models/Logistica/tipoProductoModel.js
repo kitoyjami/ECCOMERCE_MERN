0c01be1a2debc6e3d335ea241fd71438c85c8332
kitoyjami <weurbanor@unitru.edu.pe>
2024-07-03 03:13:42 -0500

    add filters

--- models/Logistica/tipoProductoModel.js
+++ models/Logistica/tipoProductoModel.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const tipoProductoSchema = new mongoose.Schema({
-    nombre: String // e.g., 'Producto', 'Servicio'
+    nombre: String,
+    tipoInterno: { 
+      type: String, 
+      enum: ['Producto', 'Servicio'] 
+    },
   });
   
   const TipoProducto = mongoose.model('TipoDProducto', tipoProductoSchema);
